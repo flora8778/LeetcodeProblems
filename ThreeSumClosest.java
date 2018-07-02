dd4d03f85a91e007512c10c4401a176b60178678
Flora <fengj@my.erau.edu>
2018-07-02 13:37:08 -0700

    threeSumCloset problem
    complete.

--- ThreeSumClosest.java
+++ ThreeSumClosest.java
@@ -1,3 +1,6 @@
+
+import java.util.Arrays;
+
 /**
  * 
  * @author florafeng
@@ -8,5 +11,59 @@
  */
 
 public class ThreeSumClosest {
+	public int threeSumClosest(int[] arr, int target){
+		Arrays.sort(arr);
 
+		int diff = Integer.MAX_VALUE;
+		int result = 0;
+
+		if(arr.length<3 || arr== null){
+			return result;
+		}
+
+		for(int i=0; i<arr.length-2; i++){
+			int j=i+1;
+			int k=arr.length-1;
+			while(j<k){
+
+				if(arr[i] + arr[j] + arr[k] == target ){
+					return arr[i] + arr[j] + arr[k];
+				}
+
+				if(arr[i] + arr[j] + arr[k] < target){
+					j++;
+				}
+				if(arr[i] + arr[j] + arr[k] > target){
+					k--;
+				}
+				if(arr[i] + arr[j] + arr[k] - target < diff){
+					diff = arr[i] + arr[j] + arr[k] - target;
+					result = arr[i] + arr[j] + arr[k];
+				}
+			}
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		ThreeSumClosest in = new ThreeSumClosest();
+		int[] arr = { -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+		int l1 = in.threeSumClosest(arr, 1);
+		System.out.println("The sum closet to target is " + l1);
+
+	}
 }
+
+/*
+ * Time complexity is O(n^2)
+ */
+
+
+
+
+
+
+
+
+
+ 
