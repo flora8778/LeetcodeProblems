f995a4df94801df9696ea0fe55a4db845bf74192
Flora <fengj@my.erau.edu>
2018-07-09 15:47:15 -0700

    candy

--- Candy.java
+++ Candy.java
@@ -10,28 +10,29 @@
  */
 
 public class Candy {
-	public int candy(int[] rating){
-		if(rating.length<1 && rating == null){
+		public int candy(int[] rating){
+		if(rating.length==0 && rating == null){
 			return 0;
 		}
-
 		int[] candies = new int[rating.length];
 
-		for(int i=0; i<rating.length;i++){
-			if(rating[i] > rating[i+1]){
-				candies[i] = candies[i+1] +1;
-			}
-			if(rating[i] < rating[i+1]){
-				candies[i+1] = candies[i] +1;
-			}
-			else{
-				candies[i]+1;
+		for(int i=1; i<rating.length;i++){
+			if(rating[i] > rating[i-1] && candies[i] <= candies[i-1]){
+				candies[i] = candies[i-1] + 1;
 			}
 		}
-
-
+		// case that second last one is 1 candy, also rating > last one rating. 
+		// so should do once more from right to left.
+		for(int i=rating.length-2; i>=0;i--){
+			if(rating[i] > rating[i+1] && candies[i]<=candies[i+1]){
+				candies[i] = candies[i+1] + 1;
+			}
+		}
+		
+		int sum = candies.length; //this is to assign each kid 1 candy.
 		for(int i=0; i<candies.length;i++){
-			if(candies[i])
+			sum+=candies[i];
 		}
+		return sum;
 	}
 }
